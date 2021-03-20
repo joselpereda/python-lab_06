0ea7d69d11c6beeaa8a4401b3fd1f4bffea31359
Jose Pereda <joselpereda@hotmail.com>
2021-03-20 14:15:06 -0400

    ladded L6_8.py - definition of a python class

---
+++ L6_8.py
@@ -0,0 +1,7 @@
+#6_8 Creating a simple class
+class my_first_class():
+    pass                        #a class must have a statement
+
+#create the object
+myClass = my_first_class()
+print(type(myClass))
